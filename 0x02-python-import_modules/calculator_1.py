68283a6aff5b553120d74359cd816ef584e99d0c
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-01-05 16:36:05 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+     Args:
+     a: first integer
+     b: second integer
+
+     Returns the value. a + b 
+     """
+
+     return (a + b)
+ def sub(a, b):
+     """MY subtraction function
+
+     Args:
+     a: first integer
+     b: second integer
+
+     Returns the value .a - b
+     """
+     return (a - b)
+ def mul(a, b):
+     """My multip,ication function
+
+     Args:
+     a:first integer
+     b: second integer
+
+     Return the value . a * b
+     """
+     return (a * b)
+ def div(a, b):
+     """My division function
+
+     Args: 
+     a: first integer
+     b: second integer
+
+     return the value . a / b
+     """
+     return int(a / b)
