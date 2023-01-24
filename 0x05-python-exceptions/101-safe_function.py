5dfddd90a14a110f30caa5c1bdf6b9af166f5ec0
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-01-23 19:50:11 -0800

    task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (TypeError, ValueError, IndexError, ZeroDivisionError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
+
