6d1de53326c87443d9d68907f4320be20f7e06b2
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-01-24 20:38:35 -0800

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,12 +15,12 @@ class Square:
     @size.setter
     def size(self, value):
         """Property setter to set size"""
-        if type(value) != inte:
+        self.__size = value
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        
 
     def area(self):
         """Public instance method that return the current area of the Square"""
