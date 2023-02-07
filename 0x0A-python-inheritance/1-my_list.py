13bc4565751c7bf06b30e2889696f50e79460c52
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-02-06 19:05:05 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+write  class MyList that inherits from list
+"""
+
+
+class MyList(lists):
+    """
+    inherited class
+    """
+    pass
+
+    def print_sorted(self):
+        """prins the sorted list
+        """
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Class Rectangle
+"""
+
+
+BaseGeometry = __import__("7-
