8701d700791e6ef32486c90c64e445ccb84e2c7b
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-02-06 17:55:31 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+
+    def area(self):
+        """
+        instantiation
+        """
+        raise Exception("area() is not implemented")
