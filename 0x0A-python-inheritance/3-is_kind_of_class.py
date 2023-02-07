58e9ffbc6655eaccb7881741ab5d20d527cab887
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-02-06 17:35:11 -0800

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+My class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    my function
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
