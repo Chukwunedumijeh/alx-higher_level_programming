a7c388c53b9ab6ad3725eb3ffc566ea97608fa6f
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-02-07 17:00:18 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""read file
+"""
+
+
+def read_file(filename=""):
+    """function that read from a file
+    """
+
+    with open(filename, 'r', encoding="utf-8") as readFile:
+        print(readFile.read(), end="")    
