724909e172210b59cadeed22b6b213a52489f720
ogbonna chinedu <chinedukingsley54@gmail.com>
2023-02-07 19:19:28 -0800

    mandatory task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,14 @@
 """add_item
 """
 import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_files
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-if __name__ == "__main__":
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.arg[1:])
-    save_to_json_file(items, "add_item.json")
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+argc = len(sys.argv)
+for idx in range(1, argc):
+     items.append(sys.argv[idx])
+save_to_json_file(items, "add_item.json")
